c769f0610a5951706231ce14d28e4ec76ff8e973
andresarangopro <andresarangopro@gmail.com>
2018-09-21 15:40:04 -0500

    asd

---
+++ parcial.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 21 14:11:31 2018
+95: 1.96
+90:1.64
+99:2.58
+@author: s207e19
+"""
+
+from Distribuciones import *
+from Intervalos import *
+from PruebaHipote import *
+from scriptPH import *
+
+print(calcularTamNU(2.58,165,50))
+print(peh(25,5117,1886,5000))
+print(hallaPUC(1.96))
+difDeMedias(100,10.3,100,5.2,False)
